ddf63505fe3d64a3f159f60d457f255718864622
Grisha Lyukshin <grigoriy.lyukshin@hotmail.com>
2018-12-13 19:06:46 -0800

    Revert "assignment with tests passed"
    this reverts commit 60d988b79f57d1ef053012d5e7b4d22d04c0faea.

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -4,56 +4,51 @@ contract SimpleBank {
     //
     // State variables
     //
-
+    
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) private balances;
+    mapping (address => uint) balances;
     
     /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
-    mapping (address => bool) public enrolled;
+    mapping (address => bool) enrolled;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
-    address public owner;
+    address owner;
     
     //
     // Events - publicize actions to external listeners
     //
     
     /* Add an argument for this event, an accountAddress */
-    event LogEnrolled(address accountAddress);
+    event LogEnrolled();
 
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade(address accountAddress, uint amount);
+    event LogDepositMade();
 
     /* Create an event called LogWithdrawal */
     /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
-    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
+
 
     //
     // Functions
     //
 
     /* Use the appropriate global variable to get the sender of the transaction */
-    constructor() public {
+    constructor() {
         /* Set the owner to the creator of this contract */
-        owner = msg.sender;
     }
 
     /// @notice Get balance
     /// @return The balance of the user
     // A SPECIAL KEYWORD prevents function from editing state variables;
     // allows function to run locally/off blockchain
-    function balance() public view returns (uint) {
+    function balance() public returns (uint) {
         /* Get the balance of the sender of this transaction */
-        uint remainingBalance = balances[msg.sender];
-        return remainingBalance;
     }
 
     /// @notice Enroll a customer with the bank
     /// @return The users enrolled status
     // Emit the appropriate event
     function enroll() public returns (bool){
-        enrolled[msg.sender] = true;
-        emit LogEnrolled(msg.sender);
     }
 
     /// @notice Deposit ether into bank
@@ -61,13 +56,9 @@ contract SimpleBank {
     // Add the appropriate keyword so that this function can receive ether
     // Use the appropriate global variables to get the transaction sender and value
     // Emit the appropriate event    
-    function deposit() public payable returns (uint) {
+    function deposit() public returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
-        address sen = msg.sender;
-        uint depositAm = msg.value;
-        balances[sen] += depositAm;
-        emit LogDepositMade(sen, depositAm);
     }
 
     /// @notice Withdraw ether from bank
@@ -80,10 +71,6 @@ contract SimpleBank {
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
            return the user's balance.*/
-        address userAdd = msg.sender;
-        require(withdrawAmount <= balances[userAdd]);
-        uint newBalance = balances[userAdd] -= withdrawAmount;
-        emit LogWithdrawal(userAdd, withdrawAmount, newBalance);
     }
 
     // Fallback function - Called if other functions don't match call or
